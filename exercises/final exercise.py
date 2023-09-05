d8c0d1f326ba6bf277e64cadd796603f8a6509b6
Pocklet <silcockcaleb@gmail.com>
2023-09-05 14:13:04 +1200

    finished

--- exercises/final exercise.py
+++ exercises/final exercise.py
@@ -12,7 +12,6 @@ data = pd.read_csv(
     skiprows=[1],
 )
 
-print(data.head())
 
 
 # creates new tavg column and cleans data
@@ -27,16 +26,55 @@ data["TMIN"] = data["TMIN"].astype(float)
 data["TAVG_NEW"] = (data["TMAX"]+data["TMIN"])/2
 # replaces na values with newly calculated vales
 data["TAVG"].fillna(data["TAVG_NEW"], inplace = True)
-print(data.head())
 
 
-# defines celius to fahr function
-def celsius_to_fahr(temp):
-    return 9 / 5 * temp + 32
+
+# defines fahr to celsius function
+def fahr_to_celsius(temp):
+    return (temp-32) * (5/9)
 
 
 # converts tavg to celsius
 data["T_C_AVG"] = ""
-data["T_C_AVG"] = celsius_to_fahr(data["TAVG"])
-print(data.head())
+data["T_C_AVG"] = fahr_to_celsius(data["TAVG"])
+
+
+# slices date into year for grouping
+data["TIME_STR"] = data["DATE"].astype(str)
+data["YEAR"] = data["TIME_STR"].str.slice(
+    start = 0, stop = 4,).astype(int
+)
+
+
+# subsets and groups by year
+data_subset = data[["YEAR", "T_C_AVG"]]
+grouped = data_subset.groupby(by=["YEAR"])
+# calculates mean for each year
+yearly_mean = grouped.mean()
+
+# prints yearly mean to show highest values
+print(yearly_mean.sort_values(by="T_C_AVG",
+ascending = False))
+
+
+# creates plot
+ax = yearly_mean.plot(
+    style = "r:",
+    title = "Finland Yearly Mean Temperature",
+    xlabel = "Year",
+    ylabel = "Temperature [°C]",
+    figsize = (12, 6),
+)
+
+# adds text to plot
+ax.text(2004, 3.40, "Warmest Year ->")
+ax.text(1916, -4.1, "<- Coldest Year")
+
+# saves plot
+plt.savefig("finland-yearly-temp-mean.pdf", 
+dpi = 600)
+
+
+
+
         
